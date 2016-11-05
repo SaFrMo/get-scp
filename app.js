289b121e81c86874065c7daf902fd1e9827e459c
Sander <sandermoolin@gmail.com>
2016-11-05 11:14:15 -0400

    Initial

---
+++ app.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const	http		= require('http');
+
+const getScp = function( index ){
+	
+	index = index || process.env.npm_config_index || process.env.npm_config_i || 1440;
+	
+	// Search cache for saved SCP
+	// If we have a saved version, try to check for any updates
+	
+	// If we don't have a cached version
+	let options = {
+		host: 'www.scp-wiki.net',
+		path: `/scp-${index}`
+	}
+	
+	// Make request
+	let request = http.request(options, (res) => {
+		
+		let data = '';
+		res.on('data', (chunk) => {
+			data += chunk;
+		});
+		res.on('end', () => {
+			console.log(data);
+		});
+		
+	});
+	
+	request.on('error', (err) => {
+		console.log(err.message);
+	});
+	
+	request.end();
+	
+	console.log(options.path);	
+}
+
+getScp();
