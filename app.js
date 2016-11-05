8c80f1dde7b5d5c1b314bd4594264dadb6e8959e
Sander <sandermoolin@gmail.com>
2016-11-05 12:05:59 -0400

    Working on local install (full page)

--- app.js
+++ app.js
@@ -1,26 +1,18 @@
 "use strict";
 
 const	http		= require('http'),
+		async		= require('async'),
 		express		= require('express'),
 		app			= express();
 
 app.get('/', (req, res) => {
-	res.send('Hello world!');
+	res.send('get-scp is up and running! Try entering a number after the "/" in the URL.');
 });
 
-app.listen(3000, () => {
-	console.log('Listening on 3000');
-});
+app.get('/:index', (req, res) => {
 
+	let index = req.params.index;
 
-
-
-
-
-const getScp = function( index ){
-	
-	index = index || process.env.npm_config_index || process.env.npm_config_i || 1440;
-	
 	// Search cache for saved SCP
 	// If we have a saved version, try to check for any updates
 	
@@ -31,23 +23,30 @@ const getScp = function( index ){
 	}
 	
 	// Make request
-	let request = http.request(options, (res) => {
+	let request = http.request(options, (httpResponse) => {
+		
+		console.log('Loading');
 		
 		let data = '';
-		res.on('data', (chunk) => {
+		httpResponse.on('data', (chunk) => {
 			data += chunk;
+			console.log('Loading...');
 		});
-		res.on('end', () => {
-			console.log(data);
+		httpResponse.on('end', () => {
+			res.send(data);
 		});
 		
 	});
 	
 	request.on('error', (err) => {
 		console.log(err.message);
+		request.end();
 	});
 	
 	request.end();
 	
-	console.log(options.path);	
-}
+});
+
+app.listen(3000, () => {
+	console.log('Listening on 3000');
+});
